c457e594185dba03c942ae8d1e6c4348a1d4b4f1
Spitfire-Ice <whyteiceua@gmail.com>
2018-09-03 21:41:12 +0300

    sds

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# webpack-starter
+Webpack
+
+Commands:
+
+    "dev": "webpack-dev-server --open --config webpack.dev.js",
+    "prod": "rimraf dist && webpack -p --config webpack.prod.js",
+    "lint": "npm run lint:js && npm run lint:css",
+    "server:prod": "http-server dist -o"
---
+++ webpack.config.js
@@ -0,0 +1,130 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const ExtractSCSS = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[hash].js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true },
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules\/(?!(dom7|swiper)\/).*/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractSCSS.extract({
+          fallback: 'style-loader',
+          use: [{
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              outputPath: 'src/',
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+              outputPath: 'src/',
+            },
+          }],
+        }),
+      },
+      {
+        test: /\.(png|jpeg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'img/',
+              name: '[name].[ext]',
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(ttf|eot|otf|woff|woff2|svg|ico)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'fonts/',
+              name: '[name].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+    }),
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new ExtractSCSS({
+      filename: 'css/[name].[hash].css',
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: path.join(__dirname, 'src', 'img/favicon'),
+        to: 'img/favicon',
+      },
+    ]),
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        ecma: 8,
+        warnings: false,
+        output: {
+          comments: false,
+          beautify: false,
+        },
+        toplevel: false,
+        nameCache: null,
+        ie8: false,
+        keep_classnames: undefined,
+        keep_fnames: false,
+        safari10: false,
+      },
+    }),
+    new OptimizeCssAssetsPlugin(),
+  ],
+};
